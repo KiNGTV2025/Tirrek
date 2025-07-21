ddb77721e40ff7633d8bc0c4af1299dd039decf7
KiNGTV2025 <goldtowerumit@gmail.com>
2025-07-22 02:46:02 +0300

    Update m3u.js

--- api/m3u.js
+++ api/m3u.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch("https://raw.githubusercontent.com/KiNGTV2025/Tirrek/main/1Umittv.m3u");
+    const response = await fetch("https://raw.githubusercontent.com/KiNGTV2025/Tirrek/main/1UmitTV.m3u");
     const text = await response.text();
     const lines = text.split("\n").map(line => line.trim());
 
