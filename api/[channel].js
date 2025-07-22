daf7742f0f4cca08d4bb69550530a3fac2eca6b7
KiNGTV2025 <goldtowerumit@gmail.com>
2025-07-23 01:04:23 +0300

    Update [channel].js

--- api/[channel].js
+++ api/[channel].js
@@ -1,10 +1,17 @@
-import fetch from "node-fetch";
-
 const channels = {
   "trt1": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukbeinsports1/playlist.m3u8",
   "trt2": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukbeinsports2/playlist.m3u8",
   "beinmax1": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukbeinsportsmax1/playlist.m3u8",
-  // ... diğer kanallar
+  "beinmax2": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukbeinsportsmax2/playlist.m3u8",
+  "smartspor": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcuksmartspor/playlist.m3u8",
+  "smartspor2": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcuksmartspor2/playlist.m3u8",
+  "ssport": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukssport/playlist.m3u8",
+  "ssport2": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukssport2/playlist.m3u8",
+  "eurosport1": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukeurosport1/playlist.m3u8",
+  "eurosport2": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukeurosport2/playlist.m3u8",
+  "sf1": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcuksf1/playlist.m3u8",
+  "tabiispor": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcuktabiispor/playlist.m3u8",
+  "aspor": "https://alpha.cf-worker-9c77973d29ec8a.workers.dev/live/selcukaspor/playlist.m3u8"
 };
 
 export default async function handler(req, res) {
