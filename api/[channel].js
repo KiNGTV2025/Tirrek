edc78b488cf0054c9bbe6850e793e690f4f71ce6
KiNGTV2025 <goldtowerumit@gmail.com>
2025-07-23 01:35:02 +0300

    Update [channel].js

--- api/[channel].js
+++ api/[channel].js
@@ -1,22 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { channel } = req.query;
 
   try {
     const filePath = path.join(process.cwd(), 'api', 'links.json');
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const links = JSON.parse(fileContents);
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const links = JSON.parse(fileData);
 
     const data = links[channel];
-    if (!data?.url) {
+    if (!data || !data.url) {
       return res.status(404).json({ error: "Kanal bulunamadı" });
     }
 
     const stream = `${data.url}|referer=${data.ref}&|user-agent=${data.user_agent}`;
     return res.status(200).json({ stream });
-
   } catch (err) {
     return res.status(500).json({ error: "Sunucu hatası", details: err.message });
   }
